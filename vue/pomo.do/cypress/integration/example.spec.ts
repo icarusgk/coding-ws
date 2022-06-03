6573e08d17e1bb8ab173a236da53cea80a2e0350
icarusgk <icarus.gk@gmail.com>
2022-06-03 10:42:24 -0600

    Remove pomo.do project

--- vue/pomo.do/cypress/integration/example.spec.ts
+++
@@ -1,9 +0,0 @@
-// https://docs.cypress.io/api/introduction/api.html
-
-// Basic Cypress test
-describe("My First Test", () => {
-  it("visits the app root url", () => {
-    cy.visit("/");
-    cy.contains("h1", "You did it!");
-  });
-});
