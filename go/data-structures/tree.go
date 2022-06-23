578abfa66106bd5f837737d939215656e26716a1
Roger <38413630+icarusgk@users.noreply.github.com>
2022-06-23 11:02:34 -0600

    Add print function
    Create a new function for printing and move the fmt.Println()

--- go/data-structures/tree.go
+++ go/data-structures/tree.go
@@ -12,8 +12,12 @@ type Tree struct {
 	root *Box
 }
 
+func (tree *Tree) print() {
+	fmt.Printf("%+v\n", *tree.root)
+}
+
 func main() {
 	tree := Tree{}
 	tree.root = &Box{data: "root"} // Not necessary a string
-	fmt.Printf("%+v", tree.root)
+	tree.print()
 }
