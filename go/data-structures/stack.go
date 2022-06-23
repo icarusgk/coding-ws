5112e4cdb9545aa93fc1a2a12225f637b91621de
Roger <38413630+icarusgk@users.noreply.github.com>
2022-06-23 11:24:49 -0600

    Add basic stack structure

---
+++ go/data-structures/stack.go
@@ -0,0 +1,21 @@
+package main
+
+// TODO
+// Push: Add an element to the top of a stack
+// Pop: Remove an element from the top of a stack
+// IsEmpty: Check if the stack is empty
+// IsFull: Check if the stack is full
+// Peek: Get the value of the top element without removing it
+
+type Plate struct {
+	data interface{}
+	next *Plate
+}
+
+type Stack struct {
+	top *Plate
+}
+
+func main() {
+
+}
